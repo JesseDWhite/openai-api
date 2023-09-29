698ace15ae34830a11ea427aa93c49bad39726db
Jesse White <jesse.white6@gmail.com>
2023-09-29 16:05:55 -0500

    add chat gpt logic

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 import random
+import openai
+import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+CHAT_KEY = os.getenv('CHAT_KEY')
+openai.api_key = CHAT_KEY 
 app = FastAPI()
 
 
@@ -9,10 +17,15 @@ async def root():
     return {"welcome": "this is the main page"}
 
 
-@app.get("/random")
-async def get_random():
-    rn = random.randint(0, 1000)
-    return {"number": rn, "limit": 1000}
+@app.get('/chat/{name}')
+async def chat(name: str):
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "user", "content": f'Hey this is {name}, let me know if this is connected.'}
+      ]
+    )
+    return response.choices[0].message.content
 
 
 @app.get("/random/{limit}")
