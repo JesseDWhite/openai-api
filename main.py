4a1904f5aed5c9686687b5fc7fafd9b56c726bc9
Jesse White <jesse.white6@gmail.com>
2023-10-02 17:19:54 -0500

    update post request

--- main.py
+++ main.py
@@ -4,26 +4,41 @@ import openai
 import json
 import os
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173"
+]
 
 class Request(BaseModel):
-    payload: str
+    data: str
 
 CHAT_KEY = os.environ.get('CHAT_KEY')
 openai.api_key = CHAT_KEY 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"welcome": "this is the main page"}
 
 
-@app.post('/chat/{payload}')
+@app.post('/chat/')
 async def chat(payload: Request):
+    payload_dict = dict(payload)
+    chat = payload_dict['data']
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=[
-        {"role": "user", "content": payload}
+        {"role": "user", "content": chat}
       ]
     )
     return response.choices[0].message.content
