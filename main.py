8337cec4b3d70801608e575c94e5c5b1e5158bf4
Jesse White <jesse.white6@gmail.com>
2023-10-09 16:05:59 -0500

    add alpha applicaion logic

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
-    "http://localhost",
-    "http://localhost:5173"
+    "http://localhost:3000",
+    "https://job-tracker-2b6b1--alpha-preview-ipt58qtt.web.app/"
 ]
+
 
 class Request(BaseModel):
     data: str
 
+
 CHAT_KEY = os.environ.get('CHAT_KEY')
-openai.api_key = CHAT_KEY 
+openai.api_key = CHAT_KEY
 app = FastAPI()
 
 app.add_middleware(
@@ -26,25 +28,75 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def root():
-    return {"welcome": "this is the main page"}
+
+@app.post('/{user_token}/extract_keywords/')
+async def resume_keywords(payload: Request, user_token: str):
+    if (user_token):
+        payload_dict = dict(payload)
+        request = payload_dict['data']
+        task = f'your task is to extract a list of hard skills and soft skills from a block of text in 550 words or less. It needs to be returned as an array in a json object with hard_skills and soft_skills as keys.'
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.5,
+            max_tokens=750,
+            messages=[
+                {"role": "system", "content": task},
+                {"role": "user", "content": request}
+            ]
+        )
+        return response.choices[0].message.content
 
 
-@app.post('/chat/')
-async def chat(payload: Request):
+@app.post('/{user_token}/interview_prep/')
+async def interview_prep(payload: Request, user_token: str):
+    if (user_token):
+        payload_dict = dict(payload)
+        job_description = payload_dict['data']
+        task = f'your task is to create 10 interview questions based on the job description provided in 550 words or less. The job description will be in the form of a block of text. It needs to be returned as an array in a json object with interview_prep as the key.'
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.5,
+            max_tokens=750,
+            messages=[
+                {"role": "system", "content": task},
+                {"role": "user", "content": job_description}
+            ]
+        )
+        return response.choices[0].message.content
+
+
+@app.post('/{user_token}/get_score/')
+async def interview_prep(payload: Request, user_token: str):
+    if (user_token):
+        payload_dict = dict(payload)
+        job_description = payload_dict['job']
+        resume = payload_dict['resume']
+        task = f'your task is to provide a score from 0 to 100 based on the qualifications on a resume compared to a job description. The resume and job desciption will be in the form of a block of text within an object.'
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.3,
+            max_tokens=750,
+            messages=[
+                {"role": "system", "content": task},
+                {"role": "user", "content": job_description}
+            ]
+        )
+        return response.choices[0].message.content
+
+
+@app.post('/write_cover_letter/')
+async def write_cover_letter(payload: Request):
     payload_dict = dict(payload)
-    chat = payload_dict['data']
+    resume = payload_dict['resume']
+    job_description = payload_dict['job_description']
+    name = payload_dict['name']
+    email = payload_dict['email']
     response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-        {"role": "user", "content": chat}
-      ]
+        model="gpt-3.5-turbo",
+        temperature=1,
+        max_tokens=300,
+        messages=[
+            {"role": "user", "content": ''}
+        ]
     )
     return response.choices[0].message.content
-
-
-@app.get("/random/{limit}")
-async def get_random(limit: int):
-    rn = random.randint(0, limit)
-    return {"number": rn, "limit": limit}
